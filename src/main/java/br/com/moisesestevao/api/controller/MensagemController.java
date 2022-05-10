d257a4dbdf9d1fdfd8069b2f810e3cbda7658ffc
Moisés Estevão <moisesestevao2004@hotmai.com>
2022-05-10 00:25:03 -0300

    anti spam for production

--- src/main/java/br/com/moisesestevao/api/controller/MensagemController.java
+++ src/main/java/br/com/moisesestevao/api/controller/MensagemController.java
@@ -3,16 +3,15 @@ package br.com.moisesestevao.api.controller;
 import br.com.moisesestevao.api.dto.MensagemDto;
 import br.com.moisesestevao.api.form.MensagemForm;
 import br.com.moisesestevao.api.model.Mensagem;
+import br.com.moisesestevao.api.model.enums.StatusLiberacao;
 import br.com.moisesestevao.api.repository.MensagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/mensagem")
@@ -20,13 +19,32 @@ public class MensagemController {
 
     @Autowired
     MensagemRepository repository;
+    StatusLiberacao status = StatusLiberacao.LIBERADO;
 
     @CrossOrigin(origins = "localhost:4200")
     @PostMapping
     public ResponseEntity<?> recebeMensagem(@RequestBody @Valid MensagemForm form){
+        if(!this.validaSpam()){
+            return ResponseEntity.badRequest().build();
+        }
         Mensagem mensagem = new Mensagem(form);
         repository.save(mensagem);
         return ResponseEntity.ok(new MensagemDto(mensagem));
+    }
+
+    private boolean validaSpam() {
+        if (this.status == StatusLiberacao.LIBERADO){
+            this.status = StatusLiberacao.BLOQUEADO;
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    status = StatusLiberacao.LIBERADO;
+                }
+            }, 5*1000);
+            return true;
+        }
+        return false;
     }
 
     @GetMapping
